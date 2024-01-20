a7a1a57689417a07cb277cbad8b41f069ce56124
andressafirmino <andressafirmino78@gmail.com>
2024-01-20 18:14:18 -0300

    feat: reset css configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import ResetStyle from './style/ResetStyle.ts'
+import GlobalStyle from './style/GlobalStyle.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 )
---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        font-family: 'Lexend Deca', sans-serif;
+        text-decoration: none;
+    }
+`
+
+export default GlobalStyle
