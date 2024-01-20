88554b70c442178a42576d013bacd0ac60fbb5b9
andressafirmino <andressafirmino78@gmail.com>
2024-01-20 18:01:57 -0300

    feat: linter setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import React from 'react';
+
 function App() {
-  
-  return (
-    <h1>
-      Fast food
-    </h1>
-  )
+  return <h1>Fast Food</h1>;
 }
 
-export default App
+export default App;
